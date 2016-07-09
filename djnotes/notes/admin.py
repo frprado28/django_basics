19cb77df23ed3a7e1a03b2adbf27c9435e5b4a22
Francis Prado <frprado28@gmail.com>
2016-07-09 16:49:21 +0800

    Initial Commit v2.0

--- djnotes/notes/admin.py
+++ djnotes/notes/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from notes.models import notes
 
 
-class NoteAdmin(admin.ModelAdmin)
+class NoteAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'title',
